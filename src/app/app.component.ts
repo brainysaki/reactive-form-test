cb7ea50a577c721bc4f507d1493620f81e2926d0
brainysaki <brainysaki@gmail.com>
2020-04-18 14:02:08 -0400

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent implements OnInit{
 
  myForm:FormGroup;
-
+//firstNamePattern = "[a-zA-Z]+";
  stepCount:number= 1;
 
 ngOnInit(){
@@ -17,7 +17,8 @@ ngOnInit(){
     {
       firstName : new FormControl('',[
         Validators.required,
-        Validators.minLength(1)
+        Validators.minLength(2),
+        Validators.pattern("^[a-zA-Z]+$")
       ]),
       lastName : new FormControl('',[
         Validators.required,
